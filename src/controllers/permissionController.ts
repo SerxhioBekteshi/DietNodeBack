d0615b402e6d936ff52f9427c7aea519f58725bd
Serxhio Bekteshi <serxhio131@gmail.com>
2023-12-28 17:44:28 +0100

    controllers of role permission needs fix

--- src/controllers/permissionController.ts
+++ src/controllers/permissionController.ts
@@ -6,28 +6,85 @@ import Menu from "../models/menuModel";
 import MenuPermission from "../models/menuPermissionsModel";
 import RolePermission from "../models/rolePermissionModel";
 import Role from "../models/roleModel";
+import { isObjEmpty } from "../utils";
 
 const getPermission = catchAsync(async (req: any, res: any, next: any) => {
   const listRoles: String[] = [];
-  const existingPermission = await Permission.findOne({ id: req.params.id });
+  const existingPermission = await Permission.findOne({
+    id: req.params.id,
+  }).lean();
 
-  console.log(existingPermission, "EXISTING PERMISSION ");
-  const roleIds = await RolePermission.find({ roleId: existingPermission.id });
-  console.log(roleIds);
-  roleIds.forEach(async (roleId: any) => {
-    const roleName = await Role.findOne({ id: roleId }).select("roleName");
-    listRoles.push(roleName);
+  if (!existingPermission) {
+    return next(new AppError("No doc permission find with that id", 404));
+  }
+  const roles = await RolePermission.find({
+    permissionId: existingPermission.id,
   });
 
-  existingPermission.roles = listRoles;
-  console.log(listRoles, existingPermission, "CHECKS IN THE END");
+  if (!roles) {
+    return next(new AppError("No roles for this permission were found", 404));
+  }
 
-  res.status(200).json({ permission: existingPermission });
+  // roles.forEach(async (role: any) => {
+  for (const role of roles) {
+    if (role.isActive) {
+      const destructedRole = await Role.findOne({ id: role.roleId });
+      listRoles.push(destructedRole.roleName);
+    }
+  }
+  existingPermission["roles"] = listRoles;
+
+  res.status(200).json(existingPermission);
 });
 
-const updatePermission = updateOne(Permission);
+const updatePermission = catchAsync(async (req: any, res: any, next: any) => {
+  if (isObjEmpty(req.body)) {
+    return next(new AppError("There are no data for updating the doc.", 400));
+  }
+  const doc = await Permission.findOneAndUpdate(
+    { id: req.params.id },
+    req.body
+  );
 
-const deletePermission = deleteOne(Permission);
+  if (!doc) {
+    return next(new AppError("No doc find with that id", 404));
+  }
+  createMenuPermission(req.body, doc, next, "update");
+  createRolePermission(req.body, doc, next);
+  res.status(200).json({ doc: doc, message: "Updated successfully" });
+});
+
+const deletePermission = catchAsync(async (req: any, res: any, next: any) => {
+  const query = await Permission.findOneAndDelete({ id: req.params.id });
+  if (!query) {
+    return next(new AppError("No doc find with that id", 404));
+  }
+
+  if (query) {
+    const rolePermissions = await RolePermission.deleteMany({
+      permissionId: req.params.id,
+    });
+
+    const menuPermissions = await MenuPermission.deleteMany({
+      permissionId: req.params.id,
+    });
+  }
+
+  // if (!rolePermissions) {
+  //   return next(
+  //     new AppError("No role permissions could be deleted, not found!", 404)
+  //   );
+  // }
+
+  // if(!menuPermissions){
+  //   return next(
+  //     new AppError("No menu permissions could be deleted, not found!", 404)
+  //   );
+  // }
+  // }
+
+  res.status(200).json("Delete successfully");
+});
 
 const createPermission = catchAsync(async (req: any, res: any, next: any) => {
   req.body.createdBy = req.user.id;
@@ -38,65 +95,78 @@ const createPermission = catchAsync(async (req: any, res: any, next: any) => {
 });
 
 const createMenuPermission = async (
-  addPermissionData: any,
-  permissionCreated: any,
-  next: any
+  permissionPayload: any,
+  permissionRow: any,
+  next: any,
+  mode?: string
 ) => {
   try {
-    if (addPermissionData.subjectId !== null) {
-      const menuItem = await Menu.findOne({ id: addPermissionData.subjectId });
-      if (menuItem) {
-        const checkRelationMenu = await MenuPermission.findOne({
-          menuId: menuItem.id,
-          permissionId: permissionCreated.id,
+    if (mode !== "edit") {
+      //we are supposing subjet Id can not be modifed for the certain permission row for the moment
+      if (permissionPayload.subjectId !== null) {
+        const menuItem = await Menu.findOne({
+          id: permissionPayload.subjectId,
         });
-        const menuPermissionBody = {
-          menuId: menuItem.id,
-          permissionId: permissionCreated.id,
-        };
-        if (checkRelationMenu) {
-          await MenuPermission.updateOne(checkRelationMenu, menuPermissionBody);
+        if (menuItem) {
+          const checkRelationMenu = await MenuPermission.findOne({
+            menuId: menuItem.id,
+            permissionId: permissionRow.id,
+          });
+
+          const menuPermissionBody = {
+            menuId: menuItem.id,
+            permissionId: permissionRow.id,
+          };
+          if (checkRelationMenu) {
+            await MenuPermission.updateOne(
+              checkRelationMenu,
+              menuPermissionBody
+            );
+          } else {
+            await MenuPermission.create(menuPermissionBody);
+          }
         } else {
-          await MenuPermission.create(menuPermissionBody);
+          return next(new AppError("No menu item was found with that id", 404));
         }
       }
     }
   } catch (err: any) {
-    console.log(err, "IN CREATE PERMISSION");
+    console.log(err, "In create Menu Permission");
     return next(new AppError(err, 500));
   }
 };
 
 const createRolePermission = async (
-  addPermissionData: any,
-  permissionCreated: any,
-  next: any
+  permissionPayload: any,
+  permissionRow: any,
+  next: any,
+  mode?: string
 ) => {
   const rolePermissions = await RolePermission.find({
-    permissionId: permissionCreated.id,
+    permissionId: permissionRow.id,
   });
 
   try {
     if (rolePermissions.length !== 0) {
       const roleIds = rolePermissions.map((role: any) => role.id);
-      await RolePermission.updateMany(
-        { id: { $in: roleIds } },
-        { $set: { isActive: 0 } }
-      );
+      // await RolePermission.updateMany(
+      //   { id: { $in: roleIds } },
+      //   { $set: { isActive: 0 } }
+      // );
     }
 
-    if (addPermissionData.roles.length !== 0) {
-      addPermissionData.roles.forEach(async (role: any) => {
-        const { roleId } = await Role.findOne({ roleName: role });
+    if (permissionPayload.roles.length !== 0) {
+      permissionPayload.roles.forEach(async (role: any) => {
+        const roleRow = await Role.findOne({ roleName: role });
 
         const rolePermissionBody = {
-          roleId: roleId,
-          permissionId: permissionCreated.id,
+          roleId: roleRow.id,
+          permissionId: permissionRow.id,
         };
 
         const checkRelationRole = await RolePermission.findOne({
-          roleId: roleId,
-          permissionId: permissionCreated.id,
+          roleId: roleRow.id,
+          permissionId: permissionRow.id,
         });
 
         if (checkRelationRole) {
@@ -115,7 +185,7 @@ const createRolePermission = async (
       });
     }
   } catch (err: any) {
-    console.log(err, "IN CREATE PERMISSION");
+    console.log(err, "In create Role Permission");
     return next(new AppError(err, 500));
   }
 };
